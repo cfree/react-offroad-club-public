ac7a77506ef1b6da52c5c472a6a24f36782591df
Craig Freeman <craigfreeman@gmail.com>
2022-04-24 11:43:26 -0600

    add meeting info

--- src/components/common/LatestEvents/LatestEvents.js
+++ src/components/common/LatestEvents/LatestEvents.js
@@ -11,6 +11,17 @@ class LatestEvents extends Component {
       <div className={styles.LatestEvents}>
         <Container>
           <h2 className={styles.heading}>Upcoming Events</h2>
+          <p>
+            Come visit us at our monthly meeting at 7pm on the second Monday of
+            each month at{' '}
+            <a
+              href="https://www.google.com/maps/dir//Charlie's+Denver,+900+E+Colfax+Ave,+Denver,+CO+80218/@39.7397694,-104.978392,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x876c792d2c23dbc9:0xb4e1b42dcd87972!2m2!1d-104.9762311!2d39.739667"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Charlie's Denver
+            </a>
+          </p>
           <StaticQuery
             query={graphql`
               query {
@@ -34,17 +45,17 @@ class LatestEvents extends Component {
                 <>
                   {upcomingEvents.length > 0 ? (
                     <ul className={styles.list}>
-                    {upcomingEvents.map((edge, index) => (
-                      <li className={styles.listItem} key={index}>
-                        <div>
-                          <Calendar date={edge.node.start} />
-                        </div>
-                        <h4 className={styles.eventHeading}>
-                          {edge.node.summary}
-                        </h4>
-                      </li>
-                    ))}
-                  </ul>
+                      {upcomingEvents.map((edge, index) => (
+                        <li className={styles.listItem} key={index}>
+                          <div>
+                            <Calendar date={edge.node.start} />
+                          </div>
+                          <h4 className={styles.eventHeading}>
+                            {edge.node.summary}
+                          </h4>
+                        </li>
+                      ))}
+                    </ul>
                   ) : (
                     <p>No events scheduled</p>
                   )}
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styles from '../assets/styles/pages/contactPage.module.scss'
+import styles from '../assets/styles/pages/contactPage.module.scss';
 import Layout from '../components/layout/Layout';
 import Main from '../components/layout/Main';
 import SEO from '../components/common/Seo';
@@ -13,6 +13,16 @@ const ContactPage = () => (
 
     <Main>
       <h2>Send Us a Message</h2>
+      <p>
+        Our monthly meetings are at 7pm on the second Monday of each month at{' '}
+        <a
+          href="https://www.google.com/maps/dir//Charlie's+Denver,+900+E+Colfax+Ave,+Denver,+CO+80218/@39.7397694,-104.978392,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x876c792d2c23dbc9:0xb4e1b42dcd87972!2m2!1d-104.9762311!2d39.739667"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Charlie's Denver
+        </a>
+      </p>
       <div className={styles.cols}>
         <ContactForm />
         <img className={styles.walkieImg} src={walkieImg} alt="Contact us" />
